85a6232e427bff6c80563a0574349f41256410c6
Nika <67195261+4321nid@users.noreply.github.com>
2021-11-16 15:22:23 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -17,3 +17,16 @@ $(function(){
 		$('.menu__list').toggleClass('menu__list--active')
 	});
 });
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+	anchor.addEventListener("click", function(event){
+		event.preventDefault();
+		const blockID = anchor.getAttribute('href')
+		document.querySelector('' + blockID).scrollIntoView({
+			behavior: "smooth",
+			block: "start"
+		})
+	})
+}
